f3b31d892bf8ed2820df6df03968cc6530b9b460
BobellaYang <78059744+BobellaYang@users.noreply.github.com>
2021-09-08 11:26:13 +0800

    Update hw1.1.py

--- HW1.1/hw1.1.py
+++ HW1.1/hw1.1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep  6 00:02:19 2021
+Created on Tue Sep  7 21:41:18 2021
 
 @author: yangbo
 """
@@ -11,189 +11,190 @@ from statistics import mean
 import pandas as pd
 from sklearn.model_selection import train_test_split  
 import numpy as np
-from scipy.optimize import fmin
+from scipy.optimize import fmin, minimize
 import matplotlib.pyplot as plt
 
 class Data:
     def __init__(self, filename):
         self.data = pd.read_json(filename)
-    
-    def show_data(self):
-        return self.data
-    
-    def normalize_data(self, df):
-        u_x = mean(df['x'])
-        u_y = mean(df['y'])
-        s_x = statistics.pstdev(df['x'])
-        s_y = statistics.pstdev(df['y'])
-        df['x'] = [(i - u_x)/s_x for i in df['x']]
-        df['y'] = [(i - u_y)/s_y for i in df['y']]
-        return df
+        self.age = pd.read_json(filename)['x']
+        self.weight = pd.read_json(filename)['y']
+        self.is_adult = pd.read_json(filename)['is_adult']
 
+    def normalization(self, x, y):
+        newx = [(i - np.mean(x))/np.std(x) for i in x]
+        newy = [(i - np.mean(y))/np.std(y) for i in y]
+        return newx, newy
 
     def partition_lineardata(self):
         #partition for linear data, only keep age less than 18 data points
-        df = self.data
-        age_18 = df['x'] <= 18
-        x = df['x'][age_18]
-        y = df['y'][age_18]
+        age_18 = self.age <= 18
+        x = self.age[age_18]
+        y = self.weight[age_18]
 
         x_train, x_rem, y_train, y_rem = train_test_split(x, y, train_size = 0.8)
         x_valid, x_test, y_valid, y_test = train_test_split(x_rem, y_rem, test_size = 0.5)
 
         return x_train, x_valid, x_test, y_train, y_valid, y_test
 
-    
-    def partition_data(self):
-        #partition for logistic regression with x is age and y is weight
-        df = self.data
-
-        x_train, x_rem, y_train, y_rem = train_test_split(df['x'], df['y'], train_size = 0.8)
-        x_valid, x_test, y_valid, y_test = train_test_split(x_rem, y_rem, test_size = 0.5)
-        return x_train, x_valid, x_test, y_train, y_valid, y_test
-
-
-    def partition_logdata(self):
-        #partition for logistic regression with x is weight and y is is_adult
-        df = self.data
-
-        x_train, x_rem, y_train, y_rem = train_test_split(df['y'], df['is_adult'], train_size = 0.8)
-        x_valid, x_test, y_valid, y_test = train_test_split(x_rem, y_rem, test_size = 0.5)
-        return x_train, x_valid, x_test, y_train, y_valid, y_test
-
-
-    def model_linear(self, x,p):
-        #define linear model equation
-        y = x*p[0] + p[1]
-        return y
-    
-    def sigmoid(self, z):
-        return 1/(1+np.exp(-z))
-    
-    def model_logistic(self, x,p):
-        #define logistic regression equation
-        y = p[0]+p[1]*(1.0/(1.0 + np.exp(-(x-p[2])/(p[3]+0.00001))))
-        return y     
-
     def loss_linear(self, p):
-        #calculate mse for linear regression 
         x_train, x_valid, x_test, y_train, y_valid, y_test = self.partition_lineardata()
-        y_pred = self.model_linear(x_train, p)
-        total = y_train - y_pred
-        MSE = np.sqrt(np.sum(np.power(total, 2)))/y_train.shape[0]
-        loss = MSE
-        return loss
-
-    def loss_log(self, p):
-        #calculate mse for logistic regression 
-        x_train, x_valid, x_test, y_train, y_valid, y_test = self.partition_data()
-        #y_pred = self.model_logistic(x_train, p)
-        #total = y_train - y_pred
-        #MSE = np.sqrt(np.sum(np.power(total, 2)))/y_train.shape[0]
-        #loss = MSE
-        left = np.multiply(-y_train, np.log(self.model_logistic(x_train, p)))
-        right = np.multiply(1-y_train, np.log(1-self.model_logistic(x_train, p)))
-        loss = np.sum(left - right) / len(x_train)
-        return loss
-
-    def loss_logistic(self, p):
-        #calculate mse for logistic regression 
-        x_train, x_valid, x_test, y_train, y_valid, y_test = self.partition_logdata()
-        y_pred = self.model_logistic(x_train, p)
-        total = y_train - y_pred
-        MSE = np.sqrt(np.sum(np.power(total, 2)))/y_train.shape[0]
-        loss = MSE
-        return loss    
+        nx,ny = self.normalization(x_train,y_train)
+        y = np.dot(nx, p[0]) + p[1]
+        total = ny - y
+        a = np.power(total, 2)
+        MSE = np.sqrt(np.sum(a))/len(ny)
+        return MSE    
     
-    def optimize(self,NFIT):
+    def optimize(self):
          #TRAIN MODEL USING SCIPY OPTIMIZER
-        po=np.random.uniform(0.5,1.,size=NFIT)
-        res = fmin(self.loss_linear, po)    
-        return res
+         po=np.random.uniform(0.5,1.,size=2)
+         res = minimize(self.loss_linear, po, method='Nelder-Mead')
+         return res.x
     
-    def optimize_log(self, NFIT):
-        po=np.random.uniform(0.5,1.,size=NFIT)
-        res = fmin(self.loss_log, po)    
-        return res        
-    
-    def optimize_logistic(self, NFIT):
-        po=np.random.uniform(0.5,1.,size=NFIT)
-        res = fmin(self.loss_logistic, po)    
-        return res        
-
-        
     def plot_linear(self):
         x_train, x_valid, x_test, y_train, y_valid, y_test = self.partition_lineardata()
         fig, ax = plt.subplots()
         ax.plot(x_train, y_train, 'o', label='Training Set')
         ax.plot(x_valid, y_valid, 'o', label='Validation Set')
         ax.plot(x_test, y_test, 'o', label='Testing Set')
+        p = self.optimize()
+        xn, yn = self.normalization(x_train,y_train)
         #the optimal value I got was below, therefore, I put the value down
-        ax.plot(x_train, 7.8579*x_train + 10.4873, label="Model")
-        ax.legend()
+        y_pred = np.dot(xn, p[0]) + p[1]
+        ax.plot(x_train, y_pred*np.std(y_train) + np.mean(y_train), label="Model")
+        a = ax.legend()
         FS=18   #FONT SIZE
         plt.xlabel('x', fontsize=FS)
         plt.ylabel('y', fontsize=FS)
 
-        plt.show()
-        
-    def plot_data(self):
-        #plotting the result with x value age, y value weight in logistic regression
-        x_train, x_valid, x_test, y_train, y_valid, y_test = self.partition_data()
-        fig, ax = plt.subplots()
-        ax.plot(x_train, y_train, 'o', label='Training Set')
-        ax.plot(x_valid, y_valid, 'o', label='Validation Set')
-        ax.plot(x_test, y_test, 'o', label='Testing Set')
-        ax.plot(x_train, self.model_logistic(x_train, self.optimize_logistic(4)), label="Model")
-        ax.legend()
-        FS=18   #FONT SIZE
-        plt.xlabel('x', fontsize=FS)
-        plt.ylabel('y', fontsize=FS)
-
-        plt.show()
-        
-    def plot_log(self):
-        #plotting the result with x value weight and y value is_adult
-        x_train, x_valid, x_test, y_train, y_valid, y_test = self.partition_logdata()
-        fig, ax = plt.subplots()
-        ax.plot(x_train, y_train, 'o', label='Training Set')
-        ax.plot(x_valid, y_valid, 'o', label='Validation Set')
-        ax.plot(x_test, y_test, 'o', label='Testing Set')
-        ax.plot(x_train, self.model_logistic(x_train, self.optimize_logistic(4)), label="Model")
-        ax.legend()
-        FS=18   #FONT SIZE
-        plt.xlabel('x', fontsize=FS)
-        plt.ylabel('y', fontsize=FS)
-
-        plt.show()
-                       
+        plt.show()    
+                          
    
     def plot_prediction(self):
         x_train, x_valid, x_test, y_train, y_valid, y_test = self.partition_lineardata()
-        y_pred = x_train*7.8579 + 10.4873
-        y_pred1 = x_valid*7.8579 + 10.4873
-        y_pred2 = x_test*7.8579 + 10.4873
-#        y_pred = self.model_linear(x_train, self.optimize(2))
- #       y_pred1 = self.model_linear(x_valid, self.optimize(2))
-  #      y_pred2 = self.model_linear(x_test, self.optimize(2))
 
+        p = self.optimize()
+        xn, yn = self.normalization(x_train,y_train)
+        #the optimal value I got was below, therefore, I put the value down
+        xt, yt = self.normalization(x_test,y_test)
+        y_pred = np.dot(xn, p[0]) + p[1]
+        y_pred1 = y_pred*np.std(y_train) + np.mean(y_train)
+        y_pred2 = np.dot(xt, p[0]) + p[1]
+        y_pred3 = y_pred2*np.std(y_test) + np.mean(y_test)
         fig, ax = plt.subplots()
-        ax.plot(y_pred, y_train, 'o', label='Training Set')
-        ax.plot(y_pred1, y_valid, 'o', label='Validation Set')
-        ax.plot(y_pred2, y_test, 'o', label='Testing Set')
+        ax.plot(y_pred1, y_train, 'o', label='Training Set')
+        ax.plot(y_pred3, y_test, 'o', label='Testing Set')
 
         ax.legend()
         FS=18   #FONT SIZE
         plt.xlabel('y predicted', fontsize=FS)
         plt.ylabel('y data', fontsize=FS)
 
-        plt.show()
+        plt.show()    
         
         
+    #Logistic regression
+    def partition_logdata(self):
+        x = self.age
+        y = self.weight
+
+        x_train, x_rem, y_train, y_rem = train_test_split(x, y, train_size = 0.8)
+        x_valid, x_test, y_valid, y_test = train_test_split(x_rem, y_rem, test_size = 0.5)
+
+        return x_train, x_valid, x_test, y_train, y_valid, y_test
+
+    def loss_log(self, p):
+        x_train, x_valid, x_test, y_train, y_valid, y_test = self.partition_logdata()
+        nx,ny = self.normalization(x_train,y_train)
+        MSE = 0
+        for i in range(len(nx)):
+            MSE += ((p[0] / (1 + np.e**((p[2] - nx[i])/p[1]))) + p[3] - ny[i])**2
+
+        return MSE    
+    
+    def optimize_log(self):
+         #TRAIN MODEL USING SCIPY OPTIMIZER
+         po=np.random.uniform(0.5,1.,size=4)
+         res = minimize(self.loss_log, po, method='Nelder-Mead')
+         return res.x
+    
+    def plot_log(self):
+        x_train, x_valid, x_test, y_train, y_valid, y_test = self.partition_logdata()
+        fig, ax = plt.subplots()
+        ax.plot(x_train, y_train, 'o', label='Training Set')
+        ax.plot(x_valid, y_valid, 'o', label='Validation Set')
+        ax.plot(x_test, y_test, 'o', label='Testing Set')
+        p = self.optimize_log()
+        xn, yn = self.normalization(x_train,y_train)
+        #the optimal value I got was below, therefore, I put the value down
+        y_pred = p[3] + p[0] / (1 + np.e**((p[2] - xn)/p[1]))
+        ax.plot(x_train, y_pred*np.std(y_train) + np.mean(y_train), label="Model")
+        a = ax.legend()
+        FS=18   #FONT SIZE
+        plt.xlabel('x', fontsize=FS)
+        plt.ylabel('y', fontsize=FS)
+
+        plt.show()    
+
+
+        
+    #classfication
+    def partition_logisticdata(self):
+        x = self.weight
+        y = self.is_adult
+
+        x_train, x_rem, y_train, y_rem = train_test_split(x, y, train_size = 0.8)
+        x_valid, x_test, y_valid, y_test = train_test_split(x_rem, y_rem, test_size = 0.5)
+
+        return x_train, x_valid, x_test, y_train, y_valid, y_test
+
+    def loss_logistic(self, p):
+        x_train, x_valid, x_test, y_train, y_valid, y_test = self.partition_logisticdata()
+        nx,ny = self.normalization(x_train,y_train)
+        y = p[3] + p[0] / (1 + np.e**((p[2] - nx)/p[1]))
+        MSE = 0
+        for i in range(len(nx)):
+            MSE += ((p[0] / (1 + np.e**((p[2] - nx[i])/p[1]))) + p[3] - ny[i])**2
+
+        return MSE    
+    
+    def optimize_logistic(self):
+         #TRAIN MODEL USING SCIPY OPTIMIZER
+         po=np.random.uniform(0.5,1.,size=4)
+         res = minimize(self.loss_logistic, po, method='Nelder-Mead')
+         return res.x
+    
+    def plot_logistic(self):
+        x_train, x_valid, x_test, y_train, y_valid, y_test = self.partition_logisticdata()
+        fig, ax = plt.subplots()
+        ax.plot(x_train, y_train, 'o', label='Training Set')
+        ax.plot(x_valid, y_valid, 'o', label='Validation Set')
+        ax.plot(x_test, y_test, 'o', label='Testing Set')
+        p = self.optimize_logistic()
+        xn, yn = self.normalization(x_train,y_train)
+        #the optimal value I got was below, therefore, I put the value down
+        y_pred = p[3] + p[0] / (1 + np.e**((p[2] - xn)/p[1]))
+        ax.plot(x_train, y_pred*np.std(y_train) + np.mean(y_train), label="Model")
+        a = ax.legend()
+        FS=18   #FONT SIZE
+        plt.xlabel('x', fontsize=FS)
+        plt.ylabel('y', fontsize=FS)
+
+        plt.show()    
+                          
+   
+                          
+   
+    
 d = Data('weight.json')
-print(d.plot_linear())
-print(d.plot_data())
-print(d.plot_log())
-print(d.plot_prediction())
+print(d.optimize())
+d.plot_linear()
+d.plot_log()
+d.plot_logistic()
+d.plot_prediction()
+plt.show()
+
+
 
 
