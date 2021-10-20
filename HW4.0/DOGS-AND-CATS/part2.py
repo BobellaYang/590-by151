e60bd7675e9c874a39b10cb36c688f934ca7f383
BellaYang <yangbo@bellayangdembp.myfiosgateway.com>
2021-10-19 23:47:40 -0400

    hw4.0

--- HW4.0/DOGS-AND-CATS/part2.py
+++ HW4.0/DOGS-AND-CATS/part2.py
@@ -106,7 +106,7 @@ model.add(layers.Dense(1, activation='sigmoid'))
 
 from tensorflow.keras import optimizers
 model.compile(loss='binary_crossentropy',
-              optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+              optimizer=optimizers.RMSprop(learning_rate=1e-4), metrics=['acc'])
 model.summary()    
     
 #PREPROCESSING
@@ -127,7 +127,7 @@ for data_batch, labels_batch in train_generator:
 
 #Fitting the model using a batch generator
 history = model.fit_generator(train_generator, steps_per_epoch=10,
-                              epochs=20, validation_data=validation_generator,
+                              epochs=10, validation_data=validation_generator,
                               validation_steps=10)
 
 model.save('cats_and_dogs_small_1.h5')
@@ -177,8 +177,55 @@ for batch in datagen.flow(x, batch_size=1):
         break
 plt.show()
 
+import numpy as np
+#Preprocessing a single image
+img_path =  os.getcwd()+'/train/dog.20.jpg'
+img = image.load_img(img_path,target_size = (150,150))
+img_tensor = image.img_to_array(img)
+img_tensor = np.expand_dims(img_tensor,axis = 0)
+img_tensor /= 255 
 
 
+layer_outputs = [layers.output for layers in model.layers[:3]]
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+activations = activation_model.predict(img_tensor)
+first_layer_activation = activations[0]
+print(first_layer_activation.shape)
+plt.matshow(first_layer_activation[0, :, :, 3], cmap='viridis')
+layer_names = []
+#Names of the layers, so you can have them as part of your plot
+for layer in model.layers[:3]:
+    layer_names.append(layer.name)
+
+images_per_row = 6
+#Displays the feature maps
+for layer_name, layer_activation in zip(layer_names, activations):
+    #Number of features in the feature map
+    n_features = layer_activation.shape[-1]
+    #The feature map has shape (l,size,size,number of features)
+    size = layer_activation.shape[1]
+    #Tiles the activation channels in this matrix 
+    n_cols = n_features // images_per_row
+    display_grid = np.zeros((size * n_cols, images_per_row * size))
+    #Tiles each filter into a big horizontal grid 
+    for col in range(n_cols):
+        for row in range(images_per_row):
+            channel_image = layer_activation[0, :, :, col * images_per_row+row]
+            #Post-processes the feature to make it visually palatable
+            channel_image -= channel_image.mean()
+            channel_image /= channel_image.std()
+            channel_image *= 64
+            channel_image += 128
+            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+            #Displays the grid
+            display_grid[col*size: (col + 1) * size,
+                         row*size: (row + 1)*size] = channel_image
+    scale = 1. / size
+    plt.figure(figsize=(scale*display_grid.shape[1], scale*display_grid.shape[0]))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+    plt.show()
 
 
 
